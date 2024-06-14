efc2bffde1016e6270999881dc5e22188b7379d9
RicherFlorian <florianrichtervi@gmail.com>
2024-06-14 13:54:29 +0200

    patch test

--- __test__/fonctions.spec.js
+++ __test__/fonctions.spec.js
@@ -1,15 +1,18 @@
 const { lancerDe } = require('../fonctions');
 
 let pos1, pos2;
-var reponse = []
 beforeAll(() => {
     pos1 = 1;
     pos2 = 1;
-    reponse = [2,3,4,5,6,7]
 }); 
 
 describe('test premier lancée dé', () => {
     it('J1', () => {   
-        expect(reponse.include(lancerDe(1))).toBe(true)
+        expect(1 < lancerDe(1, true) < 7).toBe(true)
+    })
+
+    it('J2', () => {   
+        expect(1 < lancerDe(2, true) < 7).toBe(true)
     })
 })
+
--- fonctions.js
+++ fonctions.js
@@ -1,15 +1,15 @@
-var pos1 = 0
-var pos2 = 0
+var pos1 = 1
+var pos2 = 1
 
-function lancerDe(j){
+function lancerDe(j, test){
     var de = ((Math.random() * 5 + 1)).toFixed(0)
-    
     if(j == 1) {
-        $("#case-" + pos1).css("background", "none")
+        if(!test) clearColor(pos1)
+        
         pos1 += parseInt(de) 
         if(pos1 == 50) {
-            $('#victoire').val(1)
-            $('#form-victoire').submit()
+
+            if(!test) endGame(1)
         }
         if(pos1 > 50) pos1 = 25
 
@@ -21,17 +21,17 @@ function lancerDe(j){
         if(pos1 == 2) pos1 == 17
         if(pos1 == 31) pos1 == 43
 
-        $("#case-" + pos1).css("background-color", "red")
-        $("#btn-de").attr('onClick', 'lancerDe(2)')
+        if(!test) changeColor(2, pos1)
+
         return pos1
     }
 
     else {
-        $("#case-" + pos2).css("background", "none")
+        if(!test) clearColor(pos2)
+
         pos2 += parseInt(de)
         if(pos2 == 50) {
-            $('#victoire').val(2)
-            $('#form-victoire').submit()
+            if(!test) endGame(2)
         }
 
         if(pos2 > 50) pos2 = 25
@@ -45,13 +45,30 @@ function lancerDe(j){
         if(pos2 == 31) pos2 == 43
 
         
-        $("#case-" + pos2).css("background-color", "blue")
-        $("#btn-de").attr('onClick', 'lancerDe(1)')
+        if(!test) changeColor(1, pos2)
 
         return pos2
-
     }
 }
 
+function changeColor(j, pos){
+    if(j == 1){
+        $("#case-" + pos).css("background-color", "red")
+        $("#btn-de").attr('onClick', 'lancerDe(1, false)')
+    }
+    else{
+        $("#case-" + pos).css("background-color", "blue")
+        $("#btn-de").attr('onClick', 'lancerDe(2, false)')
+    }
+}
+
+function clearColor(pos){
+    $("#case-" + pos).css("background", "none")
+}
+
+function endGame(j){
+    $('#victoire').val(j)
+    $('#form-victoire').submit()
+}
 
 module.exports = {lancerDe}
